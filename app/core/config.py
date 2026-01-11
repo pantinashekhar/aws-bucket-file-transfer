25e699fb544433c95978de71135428f625445e83
Chandrashekhar Pantina <pantinashekhar87@gmail.com>
2026-01-11 08:43:19 +0530

    config and session

--- app/core/config.py
+++ app/core/config.py
@@ -7,7 +7,7 @@ class Settings:
     PROJECT_NAME: str = "S3 Transfer Service"
     DATABASE_URL: str = os.getenv(
         "DATABASE_URL",
-        "postgresql+asyncpg://postgres:Krishna123@localhost:5432/s3_transfer_db",
+        "postgresql+asyncpg://postgres:postgres@localhost:5432/s3_transfer_db",
     )
     LOCAL_STORAGE_ROOT: str = os.getenv("LOCAL_STORAGE_ROOT", "./data")
 
