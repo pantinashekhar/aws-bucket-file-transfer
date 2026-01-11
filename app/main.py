d9bf273665b5b6c7b9b200a21ce6e3984303a76a
Chandrashekhar Pantina <pantinashekhar87@gmail.com>
2026-01-11 06:56:57 +0530

    import issue

--- app/main.py
+++ app/main.py
@@ -1,38 +1,35 @@
 from fastapi import FastAPI
-from api.routes.transfers import router as transfers_router
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi import Request
 from db.session import get_session
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
-
-
 from api.routes.transfers import router as transfers_router
-from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
-from fastapi import Request
+from api.routes.files import router as files_router
 from db.session import get_session
-from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
-
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(title="S3 Transfer Service")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(transfers_router, prefix="/api", tags=["transfers"])
+app.include_router(files_router, prefix="/api", tags=["files"])
 
 # Add after app = FastAPI(...)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
-
-# Add after app = FastAPI(...)
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
-
-
-app.include_router(transfers_router, prefix="/api")
 
 # Add after app.include_router(...)
 app.dependency_overrides[get_session] = get_session  # Enable session DI
@@ -41,3 +38,10 @@ app.dependency_overrides[get_session] = get_session  # Enable session DI
 @app.get("/")
 async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/health")
+async def health():
+    return {"status": "ok"}
+
+   
